44f5e4a88adf99480cd6b66856e6d112799895ec
lcramirezt <121839051+lcramirezt@users.noreply.github.com>
2023-01-03 15:48:25 -0500

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Laura Ramírez.
+      <time>{YEAR}</time> © Laura Ramírez. Cabinet Grotest & Pencerio by <a href="https://www.fontshare.com/">Fontshare.</a>
       <style jsx>{`
         a {
           float: right;
